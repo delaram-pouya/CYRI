4e5fa315f6fcaf2547ea623e8574ffd2006359f1
Delaram Pouyabahar <delaram@Delarams-MacBook-Pro.local>
2019-10-04 23:13:35 -0400

    random network generation

---
+++ Code/16.Random_Network.R
@@ -0,0 +1,85 @@
+### it this script, we'll try to generate a random network and 
+### then check athe accuracy of the localization prediction based on that
+
+source('Code/Functions.R')
+source('Code/Enrichment_Functions.R')
+Initialize()
+
+clean.tab <- function(x) {
+  df = data.frame(table(x$GOnumber))
+  df = df[-1,]
+  colnames(df) = c('GOterm', 'Count')
+  return(df)}
+
+
+
+##### Import data
+GOmap <- read.delim('Data/GO/go_slim_mapping.tab', header = F)
+colnames(GOmap)<- c('sys_name','stand_name','SGD_id', 'CFP','Pathway', 'GOnumber', 'type')
+Gomap_localization <- GOmap[GOmap$CFP=='C', ]
+GO_pathway_dict <- unique(Gomap_localization[,c('Pathway', 'GOnumber')])
+
+Proteins_in_each_condition.go.Count.table <- readRDS('Results/Proteins_in_each_condition.go.Count.table.rds')
+lapply(Proteins_in_each_condition.go.Count.table, head)
+
+## get a list of proetins in each condition and their degrees
+ListOfConditions <- readRDS('Data/ListOfConditions_IS7.rds')
+combined_degrees <- lapply(ListOfConditions, .getComb_Degree)
+
+Baseline <- ListOfConditions$Baseline
+listOfADs <- Baseline$ADORF
+listOfDBs <- Baseline$DBORF
+pred.score <- rep(NA, 100)
+
+
+for(i in 1:100) {
+  
+  print(i)
+  randNet <- data.frame(ADORF=sample(listOfADs), DBORF=sample(listOfDBs))
+  randNet$PPI <- paste0(randNet$ADORF, '_', randNet$DBORF)
+  rand_combined_degrees <- .getComb_Degree(randNet)
+  
+  ##### making the mapping between gene names and GO terms for the random network 
+  AD_degrees.rand <-  .getAD_Degrees(randNet)
+  DB_degrees.rand <-  .getDB_Degrees(randNet)
+  AD_Genes.rand <- data.frame(Gene=AD_degrees.rand$Gene)
+  DB_Genes.rand <- data.frame(Gene=DB_degrees.rand$Gene)
+  
+  Proteins_in_rand.df <- data.frame(Gene=unique(c(as.character(AD_Genes.rand$Gene), as.character(DB_Genes.rand$Gene))))
+  Proteins_in_rand.go <- merge(Proteins_in_rand.df, Gomap_localization, by.x='Gene', by.y='sys_name' , all.x=T, sort=F)
+  Proteins_in_rand.go.Count.table <- clean.tab( Proteins_in_rand.go)
+  
+  ##############  Predicting the localization for proteins in the random network 
+  rand_ORFS <- getCompletePairingTable(rand_combined_degrees,  randNet)
+  rand_ORFS <- getCompleteEnrichments(rand_ORFS, Proteins_in_rand.go.Count.table)
+  rand_ORFS_localized <- getFinalSubset(rand_ORFS)
+  pred.score[i] <- sum(rand_ORFS_localized$Consistent.loc >0)/nrow(rand_ORFS_localized)  # 0.06521739
+  print(pred.score[i])
+}
+
+
+pred.score <- readRDS('Results/predScores.rds')
+x = data.frame(score=c(pred.score, 0.2419355), category=c(rep('random.Net',length(pred.score)), 'baseline.prediction'))
+ggplot(x, aes(y=score, x=category))+geom_boxplot(aes(fill=category))
+
+
+
+
+baseline1 <- subset(Baseline, select=c('ADORF', 'DBORF', 'Confidence'))
+baseline1$score = baseline1$Confidence
+colnames(baseline1) = c('Bait','Prey','HEKScore','AP-MS Score')
+write.csv(baseline1, '../Desktop/cyto_test.csv', quote = F, row.names = F)
+
+
+### check this for degree controlled random network generation (is the current method label swapping?)
+# https://dnac.ssri.duke.edu/r-labs/2016/08_random_graphs.php
+# install cytoscape -> visualize -> color each node -> color with respect to visualization -> use official name
+## check the ORFs which have been labeled as consisitent in the base but are not consistent in the random network
+#### what was their neighboring proteins which led to this accurate prediction 
+
+## plot the degree distribution for both the baseline and the random network -> both with/without duplicates
+
+## for localization change prediction -> add the other pvalues as well 
+
+  
+  
--- Code/Enrichment_Functions.R
+++ Code/Enrichment_Functions.R
@@ -1,23 +1,4 @@
 
-source('Code/Functions.R')
-Initialize()
-
-##### Import data
-GOmap <- read.delim('Data/GO/go_slim_mapping.tab', header = F)
-colnames(GOmap)<- c('sys_name','stand_name','SGD_id', 'CFP','Pathway', 'GOnumber', 'type')
-Gomap_localization <- GOmap[GOmap$CFP=='C', ]
-GO_pathway_dict <- unique(Gomap_localization[,c('Pathway', 'GOnumber')])
-
-Proteins_in_each_condition.go.Count.table <- readRDS('Results/Proteins_in_each_condition.go.Count.table.rds')
-lapply(Proteins_in_each_condition.go.Count.table, head)
-
-## get a list of proetins in each condition and their degrees
-ListOfConditions <- readRDS('Data/ListOfConditions_IS7.rds')
-combined_degrees <- lapply(ListOfConditions, .getComb_Degree)
-lapply(combined_degrees, head)
-
-
-
 getListOfInteractingPairs <- function(sharedGenes , aConditionPPImat){
   sapply(1:nrow(sharedGenes), 
          function(i){
@@ -86,7 +67,7 @@ getEnriched_GOterms.global <- function(i, ORFS, Global.go.count.table){
 getTrueLocalizations <- function(localized_ORFs){
   sapply(1:nrow(localized_ORFs), 
          function(i){
-           an.ORF = base_ORFS_localized$ORF[i]
+           an.ORF = localized_ORFs$ORF[i]
            trueLocalization = Gomap_localization$GOnumber[Gomap_localization$sys_name == an.ORF]
            return(trueLocalization)}, simplify = F)
 }
@@ -135,65 +116,4 @@ getFinalSubset <- function(ORFS){
   ORFS_localized$Consistent.loc <- sapply(1:nrow(ORFS_localized), function(i) isLocalizationCorrect(i, ORFS_localized))
   return(ORFS_localized)
 }
-
-
-
-############## Baseline
-## make a list of ORFs in a condition & find their interacting pairs, map the protein names to the GO number
-base_ORFS <- getCompletePairingTable(combined_degrees$Baseline,  ListOfConditions$Baseline)
-### for all the ORFs, find the enriched GO term and its p.value 
-base_ORFS <- getCompleteEnrichments(base_ORFS, Proteins_in_each_condition.go.Count.table$Baseline)
-## subset of the protein of which a GO term has been enriched & find the true localization for the ORF accouring to GO dataset
-## check if the enriched GO term based on the interacting pairs is consistent with the true localization
-base_ORFS_localized <- getFinalSubset(base_ORFS)
-sum(base_ORFS_localized$Consistent.loc >0)/nrow(base_ORFS_localized)  # 0.2419355
-
-
-############## MMS
-mms_ORFS <- getCompletePairingTable(combined_degrees$MMS,  ListOfConditions$MMS)
-mms_ORFS <- getCompleteEnrichments(mms_ORFS, Proteins_in_each_condition.go.Count.table$MMS)
-mms_ORFS_localized <- getFinalSubset(mms_ORFS)
-sum(mms_ORFS_localized$Consistent.loc >0)/nrow(mms_ORFS_localized) # 0.2285115
-
-
-############## H2O2
-h2o2_ORFS <- getCompletePairingTable(combined_degrees$H2O2,  ListOfConditions$H2O2)
-h2o2_ORFS <- getCompleteEnrichments(h2o2_ORFS, Proteins_in_each_condition.go.Count.table$H2O2)
-h2o2_ORFS_localized <- getFinalSubset(h2o2_ORFS)
-sum(h2o2_ORFS_localized$Consistent.loc >0)/nrow(h2o2_ORFS_localized) # 0.2121896
-
-
-############## PoorCarbon
-poorcarbon_ORFS <- getCompletePairingTable(combined_degrees$PoorCarbon,  ListOfConditions$PoorCarbon)
-poorcarbon_ORFS <- getCompleteEnrichments(poorcarbon_ORFS, Proteins_in_each_condition.go.Count.table$PoorCarbon)
-poorcarbon_ORFS_localized <- getFinalSubset(poorcarbon_ORFS)
-sum(poorcarbon_ORFS_localized$Consistent.loc >0)/nrow(poorcarbon_ORFS_localized) # 0.1921708
-
-
-
-listOfGlobal_GO_enrichments = list(base_ORFS_localized, mms_ORFS_localized, h2o2_ORFS_localized, poorcarbon_ORFS_localized)
-names(listOfGlobal_GO_enrichments) <- c('baseline', 'mms', 'h2o2', 'poorcarbon')
-saveRDS(listOfGlobal_GO_enrichments, 'Results/list_Of_GO_enriched_Global_localizations.rds')
-
-
-
-# calculate 2 other p-values for comparing the GO (or the protein??) with the global amount 
-## add these 2 p.values to the table made in the other script for mms at least
-
-## use this randomized network -> not label swapping 
-# degree controlled randomization -> 
-## odd ration between the protein 
-
-
-
-####### visualization 
-
-library(igraph)
-set.seed(1)
-gs <- list()
-for (x in seq_len(20L)) {
-  gs[[x]] <- erdos.renyi.game(sample(1:100, 1), p.or.m = runif(1))
-  E(gs[[x]])$weight <- sample(1:5, ecount(gs[[x]]), T)
-}
-plot(gs[[1]], edge.width = E(gs[[1]])$weight) # plot first graph
 
